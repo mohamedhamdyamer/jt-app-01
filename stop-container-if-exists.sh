f6bc9f002e69914125ec1e3b13039746fe3e404a
Mohamed H. Amer <mohamed.hamdy.amer@gmail.com>
2024-06-04 11:08:42 +0000

    jt app 01 - C84

--- stop-container-if-exists.sh
+++ stop-container-if-exists.sh
@@ -6,5 +6,5 @@ wc=`echo $output | grep -w my-nginx-container | wc -l`
 if [[ $wc -eq 0 ]]; then
 	echo "my-nginx-container --> not found."
 else
-	sudo -S docker stop my-nginx-container
+	echo ctcvmware | sudo -S docker stop my-nginx-container
 fi
